000f1f9122b250487d06bd6869abec524a5ced1b
thir13en5 <cdmalpe123@gmail.com>
2020-02-21 12:47:32 +0000

    refined the sieve approach for python

--- 2-prime-generator.py
+++ 2-prime-generator.py
@@ -1,12 +1,26 @@
 t = int(input())
- 
 for t_i in range(t):
-    lower, upper = map(int, input().split())
-    for num in range(lower, upper + 1):  
-        if num > 1: 
-            for i in range(2, num): 
-                if (num % i) == 0: 
-                    break
-            else: 
-                print(num) 
+    # Create a boolean array "prime[0..n]" and  
+    # initialize all entries it as true. A value  
+    # in prime[i] will finally be false if i is 
+    # Not a prime, else true.
+    m, n = map(int, input().split()) 
+    prime = [True for i in range(n+1)] 
+    p = 2
+    while(p * p <= n): 
+           
+        # If prime[p] is not changed, then it is  
+       # a prime 
+        if (prime[p] == True): 
+               
+            # Update all multiples of p 
+            for i in range(p * 2, n + 1, p): 
+                prime[i] = False
+        p += 1
+    c = 0
+  
+    # Print all prime numbers 
+    for p in range(2, n+1): 
+        if prime[p] and p >= m: 
+            print(p)
     print("")
